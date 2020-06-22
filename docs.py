c3fcc22f1435cec530a4d8ac445987936bd1de68
Hagirs <60696197+Hagirs19@users.noreply.github.com>
2020-06-22 02:43:21 -0700

    Update docs.py

--- docs.py
+++ docs.py
@@ -4,20 +4,56 @@ from datetime import datetime
 
 
 class create_file():
-	def __init__(self,name_file,data=None,update=None):
+	def __init__(self,name_file,css=None,data=None,update=None):
 		self.name_file = name_file
 		self.file = open(self.name_file,"a")
+		self.css = css
 
-		if data == None:
-			self.file.write(" ")
-			self.file.close()
-		if update == True:
-			file = open(self.name_file,"w")
-			file.write(data)
-			file.close()
+		if css:
+			data = f'<link rel="stylesheet" type="text/css" href="{self.css}">'
+			self.file.write(data)
 		if data:
 			self.file.write(data)
-			self.file.close()
+		if update :
+			file = open(self.name_file,"w")
+			file.write(data)
+			
+	def start(self, title=None, favicon=None):
+		if title:
+			data =f'<!DOCTYPE html><html><head><title>{title}</title>'
+			create_file(self.name_file,css=None, data=data, update=None)
+		if favicon:
+			data =f'<link rel="shortcut icon" href="{favicon}">'
+			create_file(self.name_file,css=None, data=data, update=None)
+
+	def create_paragraf(self, text, align=None, double=None):
+		self.align = align
+		self.double = double
+		p_align = {'left':{'open':'<p align="left">','close':'</p>'},
+		'right':{'open':'<p align="right">','close':'</p>'},
+		'center':{'open':'<p align="center">','close':'</p>'},
+		'justivy':{'open':'<p align="justivy">','close':'</p>'}}
+		if align:
+			data_s = format_data[self.align]['open'] + text + format_data[self.align]['close']
+			create_file(self.name_file,css=None, data=data_s, update=None)
+
+		if double:
+			data_d = f"<{self.double}>" + format_data[self.align]['open'] + text + format_data[self.align]['close'] +f"</{self.double}>"
+			create_file(self.name_file,css=None, data=data_d, update=None)
+
+
+
+
+	def create_hyperlink(self,text,url=None, double=None):
+		self.url = url
+		self.double = double
+		if url:
+			data_c = f'<a href ="{url}">{text}</a>'
+			create_file(self.name_file,css=None, data=data_c, update=None)
+
+		if double:
+			data_d = f'<{self.double}>'+f'<a href ="{url}">{text}</a>'+f'</{self.double}>'
+			create_file(self.name_file,css=None, data=data_d, update=None)
 
 	def create_format_text(self,text,formats=None,double=None):
 		self.text = text
@@ -26,20 +62,17 @@ class create_file():
 		format_data = {'bold':{'open':'<b>','close':'</b>'},
 	    'italic':{'open':'<i>','close':'</i>'},
 	    'underline':{'open':'<u>','close':'</u>'},
-        'small':{'open':'<small>','close':'</small>'},
+            'small':{'open':'<small>','close':'</small>'},
 	    'strong':{'open':'<strong>','close':'</strong>'},
 	    'sub':{'open':'<sub>','close':'</sub>'},
 	    'sup':{'open':'<sup>','close':'</sup>'},
 	    'ins':{'open':'<ins>','close':'</ins>'},
-	    'mark':{'open':'<mark>','close':'</mark>'}
+	    'mark':{'open':'<mark>','close':'</mark>'},
+	    'code':{'open':'<code>','close':'</code>'}
 	    }
-		if formats and double == None:
-			create_file(self.name_file,data=None)
-		else:
+		if double:
+			data_d = f"<{self.double}>" + format_data[self.format]['open'] + text + format_data[self.format]['close'] +f"</{self.double}>"
+			create_file(self.name_file,css=None, data=data_d, update=None)
+		if formats:
 			data_s = format_data[self.format]['open'] + text + format_data[self.format]['close']
-			create_file(self.name_file,data=data_s)
-
-
-
-a = create_file("index.html")
-a.create_format_text("Anjing", formats="bold")
+			create_file(self.name_file,css=None, data=data_s, update=None)
